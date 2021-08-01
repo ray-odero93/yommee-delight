93b0406710bab2a4df91aa588321bac6996082c9
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-08-02 01:17:55 +0300

    Add a switch statement to carter for the sizes of pizza.

--- script.js
+++ script.js
@@ -18,6 +18,30 @@ $(document).ready(function () {
     let sizePizza = $("#sizes option:selected").val();
     let crustPizza = $("#crusts option:sewlected").val();
     let toppingPizza = [];
-    
+    $.each($("input[name='toppings']:checked"), function () {
+      toppingPizza.push($(this).val());
+    });
+
+    console.log(toppingPizza.join(", "));
+
+    switch (sizePizza) {
+      case "0":
+        price = 0;
+        break;
+      case "large":
+        price = 14.99;
+        console.log(price);
+        break;
+      case "medium":
+        price = 11.99;
+        console.log(price);
+        break;
+      case "small":
+        price = 4.79;
+        console.log(price);
+        break;
+      default:
+        console.log("ERROR!");
+    }
   })
 })
