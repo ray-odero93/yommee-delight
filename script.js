d2d2dfcf30a4c299d0b1a43a0c5722624a2582ec
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-08-02 02:17:30 +0300

    Define function that will enable the more pizza button to call the get-pizza function.

--- script.js
+++ script.js
@@ -76,6 +76,57 @@ $(document).ready(function () {
     $("#pizzaCrusts").html($("#crusts option:slected").val());
     $("#pizzaToppings").html.join(", "));
   $("#totalCost").html(total);
+
+  $("button.morePizza").click(function () {
+      let pizzaSize = $("#sizes option:selected").val();
+      let pizzaCrust = $("#crusts option:selected").val();
+      let pizzaTopping = [];
+      $.each($("input[name='toppings']:checked"), function(){            
+          pizzaTopping.push($(this).val());
+      });
+      console.log(pizzaTopping.join(", "));
+      switch(pizzaSize){
+        case "0":
+          price =0;
+        break;
+        case "large":
+           price = 14.99;
+           console.log(price);
+         break;
+         case "medium":
+           price = 11.99;
+           console.log(price);
+         break;
+         case "small":
+           price = 4.79;
+           console.log(price);
+         default:
+           console.log("ERROR!"); 
+       }
+       switch(pizzaCrust){
+          case "0":
+            crustPrice = 0;
+          break;
+          case "Crispy":
+            crustprice = 3.00;
+          break;
+          case "Stuffed":
+            crust_price = 2.49;
+          break;
+          case "Gluten-free":
+            crustPrice = 1.49;
+          break;
+          default:
+            console.log("No charge."); 
+        }
+        let topsValue = pizzaTopping.length*50;
+        console.log("toppings value" + topping_value);
+        total = price + crustPrice + topsValue;
+        console.log(total);
+    
+        checkoutTotal = checkoutTotal + total;
+        console.log(checkoutTotal);
+  });
   });
 });
   
